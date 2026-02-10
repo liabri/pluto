b73aea955b36d7dee64d066b8b1e047fe8ba5bba
liabri <liabri@tuta.io>
2026-02-10 20:39:53 +0100

    upd caddy-reverse-proxy img w/ layer4 for tcp/udp

--- modules/michel/run-public.sh
+++ modules/michel/run-public.sh
@@ -7,5 +7,6 @@ BASE="$HOME/modules/michel"
 podman run -d --replace --name rproxy-edge \
 	--network=none \
 	-v "$BASE/rproxy-edge:/etc/caddy:ro" \
+	--cap-add=NET_BIND_SERVICE \
 	localhost/caddy-reverse-proxy:latest \
 	run --config /etc/caddy/Caddyfile
